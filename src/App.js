b4c5d88042ea485991d8ee5f5cdcce40646b548d
mye181 <myeccny@gmail.com>
2022-10-29 21:38:05 -0400

    created calendar skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Calendar from "./components/Calendar";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Calendar />
     </div>
   );
 }
