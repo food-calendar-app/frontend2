04917053b3318c71e0554c37c1a823e98da51195
mye181 <mye010801@gmail.com>
2022-12-13 19:25:09 -0500

    added instructions to event modal

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -24,6 +24,9 @@ export default function EventModal() {
     const [description, setDescription] = useState(
         selectedEvent ? selectedEvent.description : ""
     );
+    const [instructions, setInstructions] = useState(
+        selectedEvent ? selectedEvent.instructions : ""
+    );
     const [selectedLabel, setSelectedLabel] = useState(
         selectedEvent ? labelsClasses.find((lbl) => lbl === selectedEvent.label) : labelsClasses[0]
     );
@@ -37,6 +40,7 @@ export default function EventModal() {
             const calendarEvent = {
                 title,
                 description,
+                instructions,
                 label: selectedLabel,
                 day: daySelected.valueOf(),
                 id: selectedEvent ? selectedEvent.id : Date.now(),
@@ -69,8 +73,8 @@ export default function EventModal() {
 
                 <div className = "p-3">
                     <div className = "grid grid-cols-1/5 items-end gap-y-7">
-                        <div>
-                            <span className = "material-icons-outlined text-gray-400">
+                        <div className = "flex flex-col justify-center items-center pb-1">
+                            <span className = "material-icons-outlined text-gray-400 text-center">
                                 restaurant
                             </span>
                         </div>
@@ -83,15 +87,19 @@ export default function EventModal() {
                             className = "pt-5 pb-1 border-0 text-gray-600 text-xl font-semibold w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-green-700"
                             onChange = {(e) => setTitle(e.target.value)}
                         />
-                        <span className = "material-icons-outlined text-gray-400">
-                            schedule
-                        </span>
+                        <div className = "flex flex-col justify-center items-center">
+                            <span className = "material-icons-outlined text-gray-400">
+                                schedule
+                            </span>
+                        </div>
                         <p className = "text-left px-3">
                             { daySelected.format("dddd, MMMM DD") }
                         </p>
-                        <span className = "material-icons-outlined text-gray-400">
-                            description
-                        </span>
+                        <div className = "flex flex-col justify-center items-center">
+                            <span className = "material-icons-outlined text-gray-400">
+                                storefront
+                            </span>
+                        </div>
                         <input 
                             required
                             type = "text" 
@@ -101,9 +109,25 @@ export default function EventModal() {
                             className = "pt-0 pb-1 border-0 text-gray-600 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-green-700"
                             onChange = {(e) => setDescription(e.target.value)}
                         />
-                        <span className = "material-icons-outlined text-gray-400">
-                            bookmark_border
-                        </span>
+                        <div className = "flex flex-col justify-center items-center">
+                            <span className = "material-icons-outlined text-gray-400">
+                                description
+                            </span>
+                        </div>
+                        <input 
+                            required
+                            type = "text" 
+                            name = "instructions" 
+                            placeholder = "Add Instructions"
+                            value = {instructions}
+                            className = "pt-0 pb-1 border-0 text-gray-600 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-green-700"
+                            onChange = {(e) => setInstructions(e.target.value)}
+                        />
+                        <div className = "flex flex-col justify-center items-center">
+                            <span className = "material-icons-outlined text-gray-400">
+                                bookmark_border
+                            </span>
+                        </div>
                         <div className = "flex justify-evenly">
                             {labelsClasses.map((lblClass, i) => (
                                 <span
