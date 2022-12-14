e721bfd97f128522d3400ad1f9926b5c49f1c6f9
mye181 <mye010801@gmail.com>
2022-12-13 19:42:07 -0500

    changed recipes page layout

--- src/components/RecipeCollection.js
+++ src/components/RecipeCollection.js
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import GlobalContext from "../context/GlobalContext";
+import CalendarHeader from "./CalendarHeader";
+import DailyList from "./DailyList";
+import EventModal from "./EventModal";
+import FavoritesModal from "./FavoritesModal";
+import Sidebar from "./Sidebar";
+import WeeklyList from "./WeeklyList";
 
 export default function RecipeCollection() {
     const [ searchInput, setSearchInput ] = useState('');
+    const [ title, setTitle ] = useState('');
+    const [ description, setDescription ] = useState('');
+    const [ instructions, setInstructions ] = useState('');
+    const { showEventModal, showDailyModal, showWeeklyModal, showFavoritesModal } = useContext(GlobalContext);
+    const { daySelected, dispatchCalEvent } = useContext(GlobalContext);
 
     const recipes = [
         { title: "blog 1", servings: "1 servings", ingredients: "mac, cheese", instructions: "Cook the macaroni. Add the cheese." },
@@ -27,35 +40,62 @@ export default function RecipeCollection() {
         )
     });
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        const calendarEvent = {
+            title,
+            description,
+            instructions,
+            label: "indigo",
+            day: daySelected.valueOf(),
+            id: Date.now(),
+        };
+        dispatchCalEvent({ type: "push", payload: calendarEvent });
+    }
+
     return (
-        <div className = "h-screen flex flex-col">
-            <div className = "flex flex-col justify-center items-center">
-                <input 
-                    type = "text"
-                    placeholder = "Search Recipe..."
-                    value = {searchInput}
-                    onChange = {handleChange.bind(this)}
-                    className = "w-1/2 mt-4 focus:ring-green-800 focus:border-green-800 focus:outline-none rounded-md"
-                />
-            </div>
-            <div className = "grid grid-cols-4 gap-10 p-6">
-                {dataSearch.map(recipe => 
-                    <div 
-                        key = {recipe}
-                        className = "border rounded-lg bg-gray-100 p-4"
-                    >
-                        <p className = "text-2xl my-3 pb-3 border border-t-0 border-x-0 border-b-2 border-green-900">{ recipe.title }</p>
-                        <div className = "grid grid-cols-3">
-                            <p className = "text-left font-bold">Serving Size:</p>
-                            <p className = "col-span-2">{ recipe.servings }</p>
-                            <p className = "text-left font-bold">Ingredients:</p>
-                            <p className = "col-span-2">{ recipe.ingredients }</p>
-                            <p className = "text-left font-bold">Instructions:</p>
-                            <p className = "col-span-2 text-left">{ recipe.instructions }</p>
+        <React.Fragment>
+            {showEventModal && <EventModal />}
+            {showDailyModal && <DailyList />}
+            {showWeeklyModal && <WeeklyList />}
+            {showFavoritesModal && <FavoritesModal />}
+
+            <div className = "h-screen flex flex-col">
+                <CalendarHeader />
+                <div className = "flex flex-1">
+                    <Sidebar />
+                    <div className = "flex flex-col">
+                        <div className = "flex justify-center items-center">
+                            <input 
+                                type = "text"
+                                placeholder = "Search Recipe..."
+                                value = {searchInput}
+                                onChange = {handleChange.bind(this)}
+                                className = "w-1/2 mt-4 focus:ring-green-800 focus:border-green-800 focus:outline-none rounded-md"
+                            />
                         </div>
-                    </div>    
-                )}
+                        <div className = "grid grid-cols-4 gap-10 p-6">
+                            {dataSearch.map(recipe => 
+                                <form 
+                                    key = {recipe}
+                                    className = "border rounded-lg bg-gray-100 p-4 group hover:bg-gray-200"
+                                >
+                                    <p className = "text-2xl my-3 pb-3 border border-t-0 border-x-0 border-b-2 border-green-800">{ recipe.title }</p>
+                                    <div className = "grid grid-cols-3">
+                                        <p className = "text-left font-bold">Serving Size:</p>
+                                        <p className = "col-span-2">{ recipe.servings }</p>
+                                        <p className = "text-left font-bold">Ingredients:</p>
+                                        <p className = "col-span-2">{ recipe.ingredients }</p>
+                                        <p className = "text-left font-bold">Instructions:</p>
+                                        <p className = "col-span-2 text-left">{ recipe.instructions }</p>
+                                    </div>
+                                </form>    
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
