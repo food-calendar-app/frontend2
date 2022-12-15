d74e4353353ec8ade852c9a48a662fae927b6192
mye181 <mye010801@gmail.com>
2022-12-15 03:27:03 -0500

    changed aboutus/home/profile for smaller displays

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,12 +10,12 @@ export default function Profile() {
     ]
 
     return (
-        <div className="relative grid grid-rows-1 grid-flow-col gap-3 bg-white max-h-screen mx-8 mt-20">
-            <div className="rounded-md bg-transparent h-2/3 ml-4 mr-0.5 my-5 row-span-2 col-span-1 drop-shadow-lg border-t">
+        <div className="relative sm:grid sm:grid-rows-1 sm:grid-flow-col sm:gap-3 bg-white max-h-screen mx-8 my-10">
+            <div className="rounded-md bg-transparent h-2/3 ml-0.5 sm:ml-4 mr-0.5 my-5 row-span-2 col-span-1 drop-shadow-lg border-t">
                 <div className="w-50 drop-shadow-lg">
                     <div className="bg-white rounded-lg px-6 py-9 mb-2 border border-gray-100">
                         <div className="photo-wrapper p-2 drop-shadow-lg">
-                            <img className="w-72 h-72 rounded-full mx-auto" src="https://images.unsplash.com/photo-1638720772346-b745bcd72f5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1177&q=80" alt="John Doe">
+                            <img className="max-w-72 max-h-72 sm:w-72 sm:h-72 rounded-full mx-auto" src="https://images.unsplash.com/photo-1638720772346-b745bcd72f5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1177&q=80" alt="John Doe">
                             </img></div>
                         <div className="p-2">
                             <h4 className="text-center text-2xl text-gray-900 font-medium leading-8">Cardi Brie</h4>
@@ -27,7 +27,7 @@ export default function Profile() {
                 <div></div>
                 <article className="flex items-center justify-between rounded-lg border border-gray-100 bg-white p-6 mb-2 mt-1">
                     <div className="flex items-center gap-4">
-                        <span className="hidden rounded-full bg-gray-100 p-2 text-gray-600 sm:block">
+                        <span className="rounded-full bg-gray-100 p-2 text-gray-600 block">
                             <span className="material-icons-outlined text-black mx-2 mt-2">
                                 dinner_dining
                             </span>
@@ -39,10 +39,8 @@ export default function Profile() {
                         </div>
                     </div>
                 </article>
-
-
             </div>
-            <div className="rounded-md bg-transparent h-2/3 ml-0.5 mr-1 my-5 row-span-2 col-span-3 drop-shadow-lg border-t">
+            <div className="rounded-md bg-transparent sm:h-2/3 ml-0.5 mr-0.5 sm:mr-1 my-5 row-span-2 col-span-3 drop-shadow-lg border-t">
                 {/* <h2 className="text-2xl mb-3">Food Details</h2>
                 <p className="text-lg">Calories:</p>
                 <p className="text-lg">Proteins:</p>
@@ -54,7 +52,7 @@ export default function Profile() {
                 {nutrition.map((item) => (
                     <article className="flex items-end justify-between rounded-lg border border-gray-100 bg-white p-6 mb-2">
                         <div className="flex items-center gap-4">
-                            <span className="hidden rounded-full bg-gray-100 p-2 text-gray-600 sm:block">
+                            <span className="rounded-full bg-gray-100 p-2 text-gray-600 block">
                                 <span className="material-icons-outlined text-black mx-2 mt-2">
                                     {item.spanner}
                                 </span>
